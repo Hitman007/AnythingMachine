fab2686a9737e153fe9ccfbadfe59e216a16572d
jim <jiminac@aol.com>
2017-05-30 04:43:35 -0700

    www

--- src/AnythingMachine/runCodeFromGithub.php
+++ src/AnythingMachine/runCodeFromGithub.php
@@ -1,14 +1,11 @@
 <?php
 
+$GithubRawURI= "https://raw.githubusercontent.com/Hitman007/AnythingMachine/master/src/AnythingMachine/HelloWorld.php";
 $ch = curl_init();
-//$header = array("Cache-Control: no-cache");
-// $header = array("Cache-Control: max-age=0");
-//curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-curl_setopt($ch, CURLOPT_URL, 'https://raw.githubusercontent.com/Hitman007/AnythingMachine/master/src/AnythingMachine/HelloWorld.php');
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($ch);
+$data = substr($str, 5);
 curl_close($ch);
-
-echo "Hi<br />";
 eval ($data);
